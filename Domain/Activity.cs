dbfdc3caaa948acf058b5449f6910a90789c1133
Nick Peachey <nick.peachey@gmail.com>
2022-02-14 17:00:07 +0000

    REAC-2 - The API controller and using postman

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Domain
+{
+    public class Activity
+    {
+        public Activity(Guid id)
+        {
+            this.Id = id;
+
+        }
+        public Guid Id { get; set; }
+
+    }
+}
