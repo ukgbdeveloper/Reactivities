8a55660eefbba3e6a66b29a327efaa3971d08f55
Nick Peachey <nick.peachey@gmail.com>
2022-02-16 11:00:38 +0000

    REAC-17 Created first query handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,30 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext context)
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
         
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            var activities = await _context.Activities.ToListAsync();
+            var activities = await _mediator.Send(new List.Query());
             return Ok(activities);
         }
 
-        [HttpGet("{id}")]
+        /*[HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             var activity = await _context.Activities.FindAsync(id);
@@ -33,6 +33,6 @@ namespace API.Controllers
                 return NotFound();
             }
             return Ok(activity);
-        }
+        }*/
     }
 }
