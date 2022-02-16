fa8af796750758520e4627d5100a40ffbe8dc699
Nick Peachey <nick.peachey@gmail.com>
2022-02-16 17:21:48 +0000

    REAC-22 Added Automapper

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -16,16 +17,18 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
                 await _context.SaveChangesAsync(cancellationToken);
                 return Unit.Value;
             }
