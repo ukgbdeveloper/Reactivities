487db1b83c47f388cd8a7c36404620fe91e17874
Nick Peachey <nick.peachey@gmail.com>
2022-02-18 10:01:32 +0000

    REAC-23 Added delete handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -45,5 +45,14 @@ namespace API.Controllers
                 Activity = activity
             }));
         } 
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command
+            {
+                Id = id
+            }));
+        } 
     }
 }
