98134b11d2227839db421458540b1d8764b101ef
Nick Peachey <nick.peachey@gmail.com>
2022-03-02 12:02:28 +0000

    REAC-38 added date format and type

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -28,8 +28,6 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const { name, value } = event.target;
-        console.log('values are');
-        console.log(name, value);
         setActivity({ ...activity, [name]: value });
     }
     return (
@@ -38,7 +36,7 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description'  value={activity.description} name='description' onChange={handleInputChange} />
                 <Form.Input placeholder='Category'  value={activity.category} name='category' onChange={handleInputChange} />
-                <Form.Input placeholder='Date'  value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input type='date' placeholder='Date'  value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City'  value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue'  value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button floated='right' positive type="submit" content='Submit' />
