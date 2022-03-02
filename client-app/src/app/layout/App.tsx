8d961d39f7d343740b3920181537025d71336753
Nick Peachey <nick.peachey@gmail.com>
2022-03-02 15:46:53 +0000

    REAC-39 REAC-40 REAC-41 added server logic axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,12 +6,15 @@ import  '../layout/styles.css';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
+import LoadingComponents from './LoadingComponents';
 
 function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     agent.Activities.list().then(response => {
@@ -21,6 +24,7 @@ function App() {
         activities.push(activity);
       });
       setActivities(activities);
+      setLoading(false);
     })
   },[]);
 
@@ -42,15 +46,34 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id ? setActivities([...activities.filter(a => a.id !== activity.id), activity]) 
-    : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if (activity.id) { 
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(a => a.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter(a => a.id !== id)]);
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(a => a.id !== id)]);
+      setSubmitting(false);
+    });
   }
+
+  if (loading) return <LoadingComponents content="Loading..." />;
 
   return (
     <>
@@ -65,7 +88,8 @@ function App() {
         openForm={handleFormOpen}
         closeForm={handleFormClose}
         createOrEdit={handleCreateOrEditActivity}
-        deleteActivity={handleDeleteActivity}/>
+        deleteActivity={handleDeleteActivity}
+        submitting={submitting}/>
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,6 +14,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, 
@@ -24,11 +25,12 @@ export default function ActivityDashboard({activities,
     openForm,
     closeForm, 
     createOrEdit, 
-    deleteActivity} : Props) {
+    deleteActivity, 
+    submitting} : Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-            <ActivityList activities={activities} 
+            <ActivityList submitting={submitting} activities={activities} 
             selectActivity={selectActivity}
             deleteActivity={deleteActivity}/>
             </Grid.Column>
@@ -38,7 +40,7 @@ export default function ActivityDashboard({activities,
                 cancelSelectActivity={cancelSelectedActivity}
                 openForm={openForm} /> }
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
