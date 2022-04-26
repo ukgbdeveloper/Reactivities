0369abdcdac7d6e80b58a9c7ff26bf915e472013
Nick Peachey <nick.peachey@gmail.com>
2022-04-26 11:07:18 +0100

    REAC-65 styled homepage and cleaned up unused refs

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,22 +1,15 @@
-import React, { SyntheticEvent, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, ItemContent, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../app/stores/store";
+
 
 interface Props {
     activity: Activity
 }
 
 export default function ActivityListItem({activity} : Props) {
-    const [target, setTarget] = useState('');
-    const {activityStore} = useStore();
-    const {deleteActivity,loading} = activityStore;
 
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
     return (
         <Segment.Group>
             <Segment>
