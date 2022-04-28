eb6d2c11c1f86b85be70e1345f21ba35d6bd1bcb
Nick Peachey <nick.peachey@gmail.com>
2022-04-28 16:21:32 +0100

    fixed linting issue

--- client-app/src/app/common/form/MyDateInput.tsx
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -12,7 +12,7 @@ export default function MyDateInput(props: Partial<ReactDatePickerProps>) {
             <DatePicker
                 {...field}
                 {...props}
-                selected={(field.value && new Date(field.value) || null)}
+                selected={((field.value && new Date(field.value)) || null)}
                 onChange={value => helpers.setValue(value)}
             />
             {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Button, Segment } from "semantic-ui-react";
+import { Link, useParams, useHistory } from "react-router-dom";
+import { Button, Header, Segment } from "semantic-ui-react";
 import LoadingComponents from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
 import { Formik , Form} from "formik";
@@ -12,11 +12,13 @@ import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
 import { Activity } from "../../../app/models/activity";
+import {v4 as uuid} from 'uuid';
 
 
 export default observer(function ActivityForm(key) {
+    const history = useHistory();
     const {activityStore} = useStore();
-    const {loading, loadActivity, loadingInitial} = activityStore;
+    const {loading, loadActivity, loadingInitial, createActivity, updateActivity} = activityStore;
     const {id} = useParams<{id:string}>();
     const [activity, setActivity] = useState<Activity>({
         id: "",
@@ -32,7 +34,7 @@ export default observer(function ActivityForm(key) {
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The activity description is required'),
         category: Yup.string().required('The activity category is required'),
-        date: Yup.string().required('The activity date is required'),
+        date: Yup.string().required('The activity date is required').nullable(),
         city: Yup.string().required('The activity city is required'),
         venue: Yup.string().required('The activity venue is required')
     })
@@ -41,35 +43,32 @@ export default observer(function ActivityForm(key) {
         if (id) loadActivity(id).then(activity => setActivity(activity!));
     }, [id, loadActivity,key]);
 
-    // function handleSumbit() {
-    //     if (activity.id.length === 0) {
-    //         let newActivity = {
-    //             ...activity,
-    //             id: uuid()
-    //         };
-    //         console.log("created");
-    //         createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
-    //     } else {
-    //         console.log("updated");
-    //         updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
-    //     }
-    // }
+    function handleFormSubmit(activity: Activity) {
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            console.log("created");
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+        } else {
+            console.log("updated");
+            updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+        }
+    }
 
-    // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    //     const { name, value } = event.target;
-    //     setActivity({ ...activity, [name]: value });
-    // }
 
     if (loadingInitial) return <LoadingComponents content="Loading activity..."/>
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal' />
             <Formik 
             validationSchema={validationSchema}
             enableReinitialize 
             initialValues={activity} 
-            onSubmit={values => console.log(values)}>
-                {({values: activity, handleChange, handleSubmit}) => (
+            onSubmit={values => handleFormSubmit(values)}>
+                {({values: activity, handleChange, handleSubmit, isValid,isSubmitting,dirty}) => (
                 <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
                     <MyTextInput name="title" placeholder="Title" />
                     <MyTextArea rows={3} placeholder='Description' name='description' />
@@ -81,9 +80,12 @@ export default observer(function ActivityForm(key) {
                         timeCaption="time"
                         dateFormat='MMMM d, yyyy h:mm aa'
                         />
+                    <Header content='Location Details' sub color='teal' />
                     <MyTextInput placeholder='City' name='city' />
                     <MyTextInput placeholder='Venue' name='venue' />
-                    <Button loading={loading} floated='right' positive type="submit" content='Submit' />
+                    <Button 
+                        disabled={isSubmitting || !dirty || !isValid }
+                        loading={loading} floated='right' positive type="submit" content='Submit' />
                     <Button as={Link} to='/activities' floated='right' type="button" content='Cancel' />
                 </Form>
                 )}
