a0034b0783c7f1b1e36c9aa19efb3757de7bd6c6
Nick Peachey <nick.peachey@gmail.com>
2022-05-04 16:07:19 +0100

    put key into config

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -22,7 +22,7 @@ public static class IdentityServiceExtensions
             .AddEntityFrameworkStores<DataContext>()
             .AddSignInManager<SignInManager<AppUser>>();
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
         {
             opt.TokenValidationParameters = new TokenValidationParameters
