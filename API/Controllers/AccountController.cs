75b075f665bca52f929cc81deece4567aee03d4f
Nick Peachey <nick.peachey@gmail.com>
2022-05-05 10:34:49 +0100

    created register endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -45,5 +46,41 @@ public class AccountController : ControllerBase
         }
 
         return Unauthorized();
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+        {
+            return BadRequest("Email Taken");
+        }
+        
+        if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+        {
+            return BadRequest("Username Taken");
+        }
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.Username
+        };
+
+        var results = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (results.Succeeded)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName
+            };
+        }
+
+        return BadRequest("Problem registering user");
     }
 }
