cec38b6419f8cde9ea5c6d07f2485031abd5928b
Nick Peachey <nick.peachey@gmail.com>
2022-05-06 14:03:19 +0100

    persisting the token and sending Bearer header

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,11 +15,26 @@ import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
 import LoginForm from '../../features/users/LoginForm';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponents from './LoadingComponents';
 
 
 function App() {
 
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponents content='Loading App...' />
+
   return (
     <>
     <ToastContainer position='bottom-right' />
