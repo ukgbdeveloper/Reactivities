c8a3a149f4bc691de4e44b06dbc3d6f0248cde2e
Nick Peachey <nick.peachey@gmail.com>
2022-05-13 10:07:20 +0100

    added migrations

---
+++ Persistence/Migrations/20220512142113_ChangedIsMainType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ChangedIsMainType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMain",
+                table: "Photos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsMain",
+                table: "Photos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+    }
+}
