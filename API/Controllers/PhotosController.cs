d6a43c79a05ab47106baa1ec9eb4e52483b269aa
Nick Peachey <nick.peachey@gmail.com>
2022-05-13 11:35:33 +0100

    completed photos section

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -15,6 +15,19 @@ public class PhotosController : BaseApiController
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(string id)
     {
-        return HandleResult(await Mediator.Send(new Delete.Command{Id = id }));
+        return HandleResult(await Mediator.Send(new Delete.Command
+        {
+            Id = id
+        }));
     }
+
+    [HttpPost("{id}/setMain")]
+    public async Task<IActionResult> SetMain(string id)
+    {
+        return HandleResult(await Mediator.Send(new SetMain.Command()
+        {
+            Id = id
+        }));
+    }
+    
 }
